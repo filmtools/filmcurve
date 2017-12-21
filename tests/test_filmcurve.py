076ba20e6b956d6ae6414ae5673d93d35c604b3d
Carsten Witt <412560+tomkyle@users.noreply.github.com>
2017-12-21 21:38:32 +0100

    Better deviation evaluation

--- tests/test_filmcurve.py
+++ tests/test_filmcurve.py
@@ -7,7 +7,7 @@ from FilmTools.FilmCurve import FilmCurve
 class TestFilmCurve(object):
 
     @pytest.mark.parametrize("target,density,expected_offset", [
-        (8, 1.29, -1.62)
+        (8, 1.29, -1.63)
     ])
     def test_findOffset(self, target, density, expected_offset):
         zones = [ 0.00, 1.00, 2.00, 3.00, 4.00, 5.00, 6.00, 7.00, 8.00, 9.00, 10.00 ]
@@ -16,11 +16,8 @@ class TestFilmCurve(object):
 
         offset = calculator.getOffset( target, density )
 
-        max_deviation = 0.01
-        ratio = 1 - (expected_offset / offset )
-
-        assert ratio <= max_deviation
-
+        max_deviation = 0.005
+        assert abs(offset - expected_offset) <= max_deviation
 
 
 
@@ -33,10 +30,9 @@ class TestFilmCurve(object):
         calculator = FilmCurve(zones, densities)
 
         zone = calculator.findZone( density )
-        ratio = 1 - (zone / expected_zone )
 
-        max_deviation = 0.01
-        assert ratio <= max_deviation
+        max_deviation = 0.005
+        assert abs(zone - expected_zone) <= max_deviation
 
 
 
@@ -49,8 +45,7 @@ class TestFilmCurve(object):
         densities = [ 0.02, 0.08, 0.17, 0.29, 0.44, 0.63, 0.86, 1.03, 1.16, 1.22, 1.34 ]
         calculator = FilmCurve(zones, densities)
 
-        zone = calculator.findDensity( zone )
-        ratio = 1 - (zone / expected_density )
+        density = calculator.findDensity( zone )
 
-        max_deviation = 0.01
-        assert ratio <= max_deviation
+        max_deviation = 0.005
+        assert abs(density - expected_density) <= max_deviation
