bb23292c93b087addbe772d827fcee64217c205c
Carsten Witt <412560+tomkyle@users.noreply.github.com>
2018-01-15 08:48:43 +0100

    Indentation

--- FilmTools/FilmCurve/FilmCurve.py
+++ FilmTools/FilmCurve/FilmCurve.py
@@ -3,112 +3,112 @@ import time
 
 class FilmCurve:
 
-	def __init__(self, zones, densities, x_precision = 12):
-		self.setDensities( densities )
-		self.setZones( zones )
-		self.x_precision = x_precision
+    def __init__(self, zones, densities, x_precision = 12):
+        self.setDensities( densities )
+        self.setZones( zones )
+        self.x_precision = x_precision
 
 
-	def setZones(self, zones):
-		self.zones = zones
-		self.createInterpolator()
+    def setZones(self, zones):
+        self.zones = zones
+        self.createInterpolator()
 
 
-	def getZones( self ):
-		return self.zones
+    def getZones( self ):
+        return self.zones
 
 
-	def setDensities( self, densities):
-		self.densities = densities
+    def setDensities( self, densities):
+        self.densities = densities
 
 
-	def getDensities( self):
-		return self.densities
+    def getDensities( self):
+        return self.densities
 
 
-	def createInterpolator( self ):
-		#
-		# Determine the 5th grade polynomial coefficients
-		# and create f(y) model function
-		#
-		zones_arr = np.array( self.getZones() )
-		densities_arr = np.array( self.getDensities() )
+    def createInterpolator( self ):
+        #
+        # Determine the 5th grade polynomial coefficients
+        # and create f(y) model function
+        #
+        zones_arr = np.array( self.getZones() )
+        densities_arr = np.array( self.getDensities() )
 
-		self.coefficients, self.residuals, _, _, _ = np.polyfit(zones_arr, densities_arr, 5, full=True)
-		self.interpolator = np.poly1d( self.coefficients )
-
-
-	def setOffset( self, offset ):
-		offset_zones = [ x + offset for x in self.zones]
-		self.setZones( offset_zones )
+        self.coefficients, self.residuals, _, _, _ = np.polyfit(zones_arr, densities_arr, 5, full=True)
+        self.interpolator = np.poly1d( self.coefficients )
 
 
-	# Find the in-fact zone for the given density
-	# and return the numeric difference to given zone
-	# as *negative* number.
-	#
-	# N.B. The result is negative as used in darkroom analysis.
-	def getOffset( self, zone, density):
-
-		found_at = self.findZone( density )
-
-		return zone - found_at
+    def setOffset( self, offset ):
+        offset_zones = [ x + offset for x in self.zones]
+        self.setZones( offset_zones )
 
 
-	#
-	# OK, thats simple
-	#
-	def findDensity( self, zone ):
-		return self.interpolator( zone )
+    # Find the in-fact zone for the given density
+    # and return the numeric difference to given zone
+    # as *negative* number.
+    #
+    # N.B. The result is negative as used in darkroom analysis.
+    def getOffset( self, zone, density):
+
+        found_at = self.findZone( density )
+
+        return zone - found_at
 
 
-	#
-	# Kind of goal seek:
-	# Find x where interpolator(x) first meets (and firstly equals)
-	# the given density y.
-	#
-	def findZone( self, density ):
-
-		#
-		# A. Experiment - is this better than the B. section below?
-		#
-
-		# zone_interpolated = np.interp( density, self.interpolator(self.zones), self.zones)
-		# return zone_interpolated
+    #
+    # OK, thats simple
+    #
+    def findDensity( self, zone ):
+        return self.interpolator( zone )
 
 
-		# B. Find good start point for x,
-		# incrementing with decreasing steps
-		# until calculated y (density) value slightly below the target value
+    #
+    # Kind of goal seek:
+    # Find x where interpolator(x) first meets (and firstly equals)
+    # the given density y.
+    #
+    def findZone( self, density ):
 
-		x = 0
-		y = None
+        #
+        # A. Experiment - is this better than the B. section below?
+        #
 
-		timeout = time.time() + 30 # 30 seconds from now
-
-		for i in range(1, self.x_precision):
-			x_increment = (10**i-1)**-1
-			while time.time() <= timeout and y <= density:
-				y = self.interpolator( x )
-				if y < density:
-					x = x + x_increment
-
-			# y now is greater than requested,
-			# so step back and calculate new (smaller) y
-			x = x - x_increment
-			y = self.interpolator( x )
+        # zone_interpolated = np.interp( density, self.interpolator(self.zones), self.zones)
+        # return zone_interpolated
 
 
-		# Now perform 'precise' goal seek,
-		# starting from last found x
-		x_increment = (10** self.x_precision)**-1
-		y = None
+        # B. Find good start point for x,
+        # incrementing with decreasing steps
+        # until calculated y (density) value slightly below the target value
 
-		while time.time() <= timeout and y <= density:
-			y = self.interpolator( x )
-			x = x + x_increment
+        x = 0
+        y = None
 
-		# x is zone
-		return x
+        timeout = time.time() + 30 # 30 seconds from now
+
+        for i in range(1, self.x_precision):
+            x_increment = (10**i-1)**-1
+            while time.time() <= timeout and y <= density:
+                y = self.interpolator( x )
+                if y < density:
+                    x = x + x_increment
+
+            # y now is greater than requested,
+            # so step back and calculate new (smaller) y
+            x = x - x_increment
+            y = self.interpolator( x )
+
+
+        # Now perform 'precise' goal seek,
+        # starting from last found x
+        x_increment = (10** self.x_precision)**-1
+        y = None
+
+        while time.time() <= timeout and y <= density:
+            y = self.interpolator( x )
+            x = x + x_increment
+
+        # x is zone
+        return x
 
 
